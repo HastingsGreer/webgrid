ac0cfe622ffdc245e42d31c08090250cbbdf3c2a
HastingsGreer <hastings.greer@gmail.com>
2022-04-01 01:42:19 +0000

    Deploying to gh-pages from @ HastingsGreer/webgrid@176df6557fa5b53b6ca9a0537aba5512a4366ba7 🚀

--- convert.py
+++
@@ -1,8 +0,0 @@
-import keras
-import tensorflowjs as tfjs
-import os
-name = input()
-
-
-model = keras.models.load_model("light_diverse_linefinder")
-tfjs.converters.save_keras_model(model, "../webgrid/static/tfjs_dir")
